95129f9b12ca7dd25c0207c2cd9c5773b0dd508f
salehbajaber777 <salehbajaber712@gmail.com>
2026-04-22 17:28:22 +0300

    Add files via upload
    برنامج تحويل العملات

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApplication7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBoxForm = new System.Windows.Forms.ComboBox();
+            this.comboBoxTo = new System.Windows.Forms.ComboBox();
+            this.textBoxAmount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(259, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Currency Conversion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(192, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Amount:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(192, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Form:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(192, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "to";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label5.Location = new System.Drawing.Point(192, 239);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "RESULT:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(263, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 35);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "CONVERT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // comboBoxForm
+            // 
+            this.comboBoxForm.FormattingEnabled = true;
+            this.comboBoxForm.Location = new System.Drawing.Point(263, 108);
+            this.comboBoxForm.Name = "comboBoxForm";
+            this.comboBoxForm.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxForm.TabIndex = 7;
+            // 
+            // comboBoxTo
+            // 
+            this.comboBoxTo.FormattingEnabled = true;
+            this.comboBoxTo.Location = new System.Drawing.Point(263, 139);
+            this.comboBoxTo.Name = "comboBoxTo";
+            this.comboBoxTo.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxTo.TabIndex = 8;
+            // 
+            // textBoxAmount
+            // 
+            this.textBoxAmount.Location = new System.Drawing.Point(263, 74);
+            this.textBoxAmount.Name = "textBoxAmount";
+            this.textBoxAmount.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAmount.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 261);
+            this.Controls.Add(this.textBoxAmount);
+            this.Controls.Add(this.comboBoxTo);
+            this.Controls.Add(this.comboBoxForm);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBoxForm;
+        private System.Windows.Forms.ComboBox comboBoxTo;
+        private System.Windows.Forms.TextBox textBoxAmount;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form
+    {
+        // قاموس لتخزين أسعار الصرف، حيث يكون المفتاح هو رمز العملة والقيمة هي سعر الصرف.
+        Dictionary<string, double> exchangeRates = new Dictionary<string, double>();
+
+        // مسار ملف أسعار الصرف.
+        string filePath = "exchangeRates";
+
+        // دالة البناء للنموذج (Form1).
+        public Form1()
+        {
+            // تهيئة مكونات النموذج.
+            InitializeComponent();
+
+            // تحميل أسعار الصرف عند بدء تشغيل البرنامج.
+            LoadExchangeRates();
+        }
+
+        // دالة لتحميل أسعار الصرف من الملف.
+        private void LoadExchangeRates()
+        {
+            // إذا كان الملف غير موجود، قم بإنشائه بالأسعار الافتراضية.
+            if (!File.Exists(filePath))
+            {
+                // كتابة الأسعار الافتراضية في الملف.
+                File.WriteAllText(filePath, "USD,3.75\nEUR,4.10\nGBP,4.85");
+
+                // عرض رسالة للمستخدم لتحديث الأسعار.
+                MessageBox.Show("تم انشاء اسعار الملف ,قم بتحديثه بالبيانات الصحيحه ,تنبيه", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            // مسح القاموس الحالي لأسعار الصرف.
+            exchangeRates.Clear();
+
+            // قراءة جميع سطور الملف.
+            string[] lines = File.ReadAllLines(filePath);
+
+            // المرور على كل سطر في الملف.
+            foreach (string line in lines)
+            {
+                // متغير لتخزين سعر الصرف.
+                double rate;
+
+                // تقسيم السطر إلى جزأين (رمز العملة وسعر الصرف).
+                string[] parts = line.Split(',');
+
+                // إذا كان السطر يحتوي على جزأين وكان سعر الصرف قابلًا للتحويل إلى عدد عشري.
+                if (parts.Length == 2 && double.TryParse(parts[1], out rate))
+                {
+                    // إضافة رمز العملة وسعر الصرف إلى القاموس.
+                    exchangeRates[parts[0].Trim()] = rate;
+                }
+            }
+
+            // مسح عناصر صندوقي الاختيار.
+            comboBoxForm.Items.Clear();
+            comboBoxTo.Items.Clear();
+
+            // إضافة رموز العملات إلى صندوقي الاختيار.
+            foreach (var currency in exchangeRates.Keys)
+            {
+                comboBoxForm.Items.Add(currency);
+                comboBoxTo.Items.Add(currency);
+            }
+
+            // تحديد العنصر الأول في صندوق الاختيار الأول.
+            if (comboBoxForm.Items.Count > 0)
+                comboBoxForm.SelectedIndex = 0;
+
+            // تحديد العنصر الثاني في صندوق الاختيار الثاني.
+            if (comboBoxTo.Items.Count > 1)
+                comboBoxTo.SelectedIndex = 1;
+        }
+
+        // دالة لمعالجة حدث النقر على زر التحويل.
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            // متغير لتخزين المبلغ المراد تحويله.
+            double amount;
+
+            // إذا كان المبلغ المدخل قابلًا للتحويل إلى عدد عشري.
+            if (double.TryParse(textBoxAmount.Text, out amount))
+            {
+                // الحصول على رمز العملة من صندوق الاختيار الأول.
+                string fromCurrency = comboBoxForm.SelectedItem.ToString();
+
+                // الحصول على رمز العملة من صندوق الاختيار الثاني.
+                string toCurrency = comboBoxTo.SelectedItem.ToString();
+
+                // إذا كانت العملتان موجودتين في القاموس.
+                if (exchangeRates.ContainsKey(fromCurrency) && exchangeRates.ContainsKey(toCurrency))
+                {
+                    // حساب المبلغ المحول.
+                    double convertedAmount = amount * (exchangeRates[toCurrency] / exchangeRates[fromCurrency]);
+
+                    // عرض المبلغ المحول في التسمية.
+                    label5.Text = convertedAmount.ToString();
+                }
+                else
+                {
+                    // عرض رسالة خطأ إذا كانت العملات غير صحيحة.
+                    MessageBox.Show("يرجى اختيار عملات صحيحه ,", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                // عرض رسالة خطأ إذا كان المبلغ غير صحيح.
+                MessageBox.Show("يرجى اختيار مبلغ صحيح ,", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        // دالة لمعالجة حدث النقر على الزر 1 (يمكنك إضافة أي كود هنا).
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // يمكنك إضافة أي كود هنا إذا كنت بحاجة إلى تنفيذ شيء ما عند النقر على button1
+        }
+    }
+}
